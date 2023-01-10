6801a85a5386c6a219689dd1b3038695aa2f0f72
Abdelkrim <kissab@gmail.com>
2023-01-10 14:27:49 +0100

    slash api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  API_URL : 'https://easycontratserver.onrender.com',
+  API_URL : 'https://easycontratserver.onrender.com/'+'api',
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  API_URL : 'https://easycontratserver.onrender.com',
+  API_URL : 'https://easycontratserver.onrender.com/'+'api',
   production: true
 };
 
