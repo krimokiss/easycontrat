920102ffd9e24f9714d324739d5dfef9dfe95122
Abdelkrim <kissab@gmail.com>
2023-01-12 10:23:36 +0100

    Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'easycontrat',
+  webDir: 'dist/easycontrat',
+  bundledWebRuntime: false
+};
+
+export default config;
